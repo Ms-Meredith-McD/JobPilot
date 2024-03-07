b55dcf618e88bd78b4e0b6c73d8eb386741e8890
RB <jsriley24505@gmail.com>
2024-03-07 12:53:45 -0600

    setup job routes and job controller

---
+++ server/routes/api/job.routes.js
@@ -0,0 +1,51 @@
+const router = require("express").Router()
+
+const {
+  getJobById,
+  createJob,
+  updateJobById,
+  deleteJobById
+} = require("../../Controllers/job.controller.js")
+
+
+router.get("/:id", async (req, res) => {
+  try {
+    const payload = await getJobById(req.params.id)
+    res.status(200).json({ status: "success", payload })
+  }catch(err){
+    console.log(err.message)
+    res.status(500).json({ status: "error", payload: err.message })
+  }
+})
+
+router.post("/", async (req, res) => {
+  try {
+    const payload = await createJob(req.body)
+    res.status(200).json({ status: "success", payload })
+  }catch(err){
+    console.log(err.message)
+    res.status(500).json({ status: "error", payload: err.message })
+  }
+})
+
+router.put("/:id", async (req, res) => {
+  try {
+    const payload = await updateJobById(req.params.id, req.body)
+    res.status(200).json({ status: "success", payload })
+  }catch(err){
+    console.log(err.message)
+    res.status(500).json({ status: "error", payload: err.message })
+  }
+})
+
+router.delete("/:id", async (req, res) => {
+  try {
+    const payload = await deleteJobById(req.params.id)
+    res.status(200).json({ status: "success", payload })
+  }catch(err){
+    console.log(err.message)
+    res.status(500).json({ status: "error", payload: err.message })
+  }
+})
+
+module.exports = router;
