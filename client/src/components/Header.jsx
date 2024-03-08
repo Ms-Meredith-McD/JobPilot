bcbd011276d3d7e55dc459d58966740fb76adf67
RB <jsriley24505@gmail.com>
2024-03-08 12:55:53 -0600

    setup navbar

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -2,9 +2,27 @@ function Header() {
 
 
     return (
-        <>
-            <h1> Hey mom, I'm home!</h1>
-        </>
+        <div className="d-flex">
+            <nav className="navbar navbar-expand-lg navbar-dark bg-dark w-100 p-2">
+  <a className="navbar-brand" href="#">JobPilot</a>
+  <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+    <span className="navbar-toggler-icon"></span>
+  </button>
+  <div className="collapse navbar-collapse" id="navbarNav">
+    <ul className="navbar-nav">
+      <li className="nav-item active">
+        <a className="nav-link" href="#">My Jobs</a>
+      </li>
+      <li className="nav-item">
+        <a className="nav-link" href="#">Resumes</a>
+      </li>
+      <li className="nav-item">
+        <a className="nav-link" href="#">Signup</a>
+      </li>
+    </ul>
+  </div>
+</nav>
+        </div>
     )
 }
 
