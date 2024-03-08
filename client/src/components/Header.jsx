58bad4bb0cde71158f0b4b25aee9a18bba973a5d
RB <jsriley24505@gmail.com>
2024-03-08 14:49:41 -0600

    fixed nav links

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -11,13 +11,13 @@ function Header() {
   <div className="collapse navbar-collapse justify-content-flex-end" id="navbarNav">
     <ul className="navbar-nav">
       <li className="nav-item active">
-        <a className="nav-link" href="#">My Jobs</a>
+        <a className="nav-link" href="/jobs">My Jobs</a>
       </li>
       <li className="nav-item">
-        <a className="nav-link" href="#">Resumes</a>
+        <a className="nav-link" href="/resume">Resumes</a>
       </li>
       <li className="nav-item">
-        <a className="nav-link" href="#">Signup</a>
+        <a className="nav-link" href="/login">Signup</a>
       </li>
     </ul>
   </div>
