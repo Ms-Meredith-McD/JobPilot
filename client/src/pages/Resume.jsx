bccafbff15ab6ded072bb1b2375952b5d83419ef
Ms-Meredith-McD <meredith.e.mcdonald@gmail.com>
2024-03-08 14:54:32 -0600

    resume pdf links

--- client/src/pages/Resume.jsx
+++ client/src/pages/Resume.jsx
@@ -1,13 +1,15 @@
-
-
-function Resume() {
-
-
+const Resume = () => {
     return (
-      <>
-  <h1> Resumes</h1>
-      </>
-    )
-  }
-  
-  export default Resume
+        <div>
+            <h1>Resumes</h1>
+            <div>
+            <a href={process.env.PUBLIC_URL + '../assets/entrylevelResume.pdf'} download>Download Entry Level Resume</a>
+            </div>
+            <div>
+            <a href={process.env.PUBLIC_URL + '../assets/midlevelResume.pdf'} download>Download Mid Level Resume</a>
+            </div>
+        </div>
+    );
+};
+
+export default Resume;
