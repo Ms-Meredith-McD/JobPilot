31f8323b6a9701969cd69990822ca52e01b43c17
Ms-Meredith-McD <meredith.e.mcdonald@gmail.com>
2024-03-08 10:47:53 -0600

    updating notes and tracker models with references

--- server/Models/Notes.js
+++ server/Models/Notes.js
@@ -3,7 +3,10 @@ const mongoose = require('mongoose');
 
 const noteSchema = new mongoose.Schema({
   body: { type: String },
-});
+  job: {
+    job: { type: mongoose.Schema.Types.ObjectId, ref: 'Job' }
+  }
+  });
 
 
 const Note = mongoose.model('Note', noteSchema);
