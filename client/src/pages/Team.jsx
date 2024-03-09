e2e5c6cb353ede968d6210c6bc37454bdfb9c11f
RB <jsriley24505@gmail.com>
2024-03-09 12:56:00 -0600

    added a icon to the site

--- client/src/pages/Team.jsx
+++ client/src/pages/Team.jsx
@@ -8,16 +8,16 @@ function Team() {
 <div className="row teamRow">
   <h2>Jonathon Blom</h2>
   <div className="col-md-6 teamCol">
-  <a href="https://github.com/blomjonathon" className="btn btn-dark">Github</a>
-  <a href="https://65ea39a90d726d77d356f060--jonathonblom.netlify.app/" className="btn btn-dark">Portfolio</a>
+  <a href="https://github.com/blomjonathon" className="btn btn-dark teamBtn">Github</a>
+  <a href="https://65ea39a90d726d77d356f060--jonathonblom.netlify.app/" className="btn btn-dark teamBtn">Portfolio</a>
   </div>
   </div>
 
   <div className="row teamRow">
   <h2>Riley Burns</h2>
   <div className="col-md-6 teamCol">
-  <a href="https://github.com/RB2277" className="btn btn-dark">Github</a>
-  <a href="https://main--superlative-otter-d6c03b.netlify.app/" className="btn btn-dark">Portfolio</a>
+  <a href="https://github.com/RB2277" className="btn btn-dark teamBtn">Github</a>
+  <a href="https://main--superlative-otter-d6c03b.netlify.app/" className="btn btn-dark teamBtn">Portfolio</a>
   </div>
   </div>
 
@@ -25,16 +25,16 @@ function Team() {
   <div className="row teamRow">
   <h2>Alex Geer</h2>
   <div className="col-md-6 teamCol">
-  <a href="https://github.com/agDesignz" className="btn btn-dark">Github</a>
-  <a href="https://alexgeer.netlify.app/" className="btn btn-dark">Portfolio</a>
+  <a href="https://github.com/agDesignz" className="btn btn-dark teamBtn">Github</a>
+  <a href="https://alexgeer.netlify.app/" className="btn btn-dark teamBtn">Portfolio</a>
   </div>
   </div>
 
   <div className="row teamRow">
   <h2>Meredith McDonald</h2>
   <div className="col-md-6 teamCol">
-  <a href="https://github.com/Ms-Meredith-McD" className="btn btn-dark">Github</a>
-  <a href="https://meredith-mcdonald-portfolio.netlify.app/" className="btn btn-dark">Portfolio</a>
+  <a href="https://github.com/Ms-Meredith-McD" className="btn btn-dark teamBtn">Github</a>
+  <a href="https://meredith-mcdonald-portfolio.netlify.app/" className="btn btn-dark teamBtn">Portfolio</a>
   </div>
   </div>
   </div>
