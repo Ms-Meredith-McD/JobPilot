6653a144e717a7780951013348de3fce6b7eb29e
RB <jsriley24505@gmail.com>
2024-03-09 15:29:20 -0600

    imported mongoose into seed file. refactored code

--- utils/seeds.js
+++ utils/seeds.js
@@ -1,54 +1,119 @@
-const jobSeeds = [
-    {
-        _id: "65eccc5ffc13ae7885cd3689",
-        company: "Lorem",
-        jobTitle: "Lorem Employee",
-        website: "https://www.lorem.com",
-        user: "65eccc5ffc13ae7885cd367e", 
-        notes: "65eccc5ffc13ae7885cd367f", 
-      },
-      {
-        _id: "65eccc5ffc13ae7885cd368d",
-        company: "Ipsum",
-        jobTitle: "Ipsum Employee",
-        website: "https://www.ipsum.com",
-        user: "65eccc5ffc13ae7885cd36a6", 
-        notes: "65eccc5ffc13ae7885cd3681", 
-      }
-    ];    
+const mongoose = require("mongoose");
+const { Job, Notes, Tracker, User } = require("../server/Models");
 
+mongoose.connect("mongodb://127.0.0.1:27017/jobpilot", {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+});
+
+const jobSeeds = [
+  {
+    _id: "65eccc5ffc13ae7885cd3689",
+    company: "Lorem",
+    jobTitle: "Lorem Employee",
+    website: "https://www.lorem.com",
+    user: "65eccc5ffc13ae7885cd367e",
+    notes: "65eccc5ffc13ae7885cd367f",
+  },
+  {
+    _id: "65eccc5ffc13ae7885cd368d",
+    company: "Ipsum",
+    jobTitle: "Ipsum Employee",
+    website: "https://www.ipsum.com",
+    user: "65eccc5ffc13ae7885cd36a6",
+    notes: "65eccc5ffc13ae7885cd3681",
+  },
+];
 
 const userSeeds = [
-    {
-          _id: "65eccc5ffc13ae7885cd367e",
-          username: "aristotle",
-          password: "password1",
-          email: "philosphy@greece.com",
-          jobs: ["65eccc5ffc13ae7885cd3689"]
+  {
+    _id: "65eccc5ffc13ae7885cd367e",
+    username: "aristotle",
+    password: "password1",
+    email: "philosphy@greece.com",
+    jobs: ["65eccc5ffc13ae7885cd3689"],
+  },
+  {
+    _id: "65eccc5ffc13ae7885cd36a6",
+    username: "socrates",
+    password: "password2",
+    email: "athens@classical.com",
+    jobs: ["65eccc5ffc13ae7885cd368d"],
+  },
+];
 
-      },
-     {
-         _id: "65eccc5ffc13ae7885cd36a6",
-         username: "socrates",
-        password: "password2",
-        email: "athens@classical.com",
-        jobs: ["65eccc5ffc13ae7885cd368d"]
+const noteSeeds = [
+  {
+    _id: "65eccc5ffc13ae7885cd36c1",
+    body: "Speaking with Alexander The Great on 4/10",
+    job: {
+      job: "65eccc5ffc13ae7885cd3689",
+    },
+  },
+  {
+    _id: "65eccc5ffc13ae7885cd36c3",
+    body: "Meeting with a publisher for my republic book on tuesday",
+    job: {
+      job: "65eccc5ffc13ae7885cd368d",
+    },
+  },
+];
 
-     }
-    ]
-
-    const noteSeeds = [
-        {
-            body: "Speaking with Alexander The Great on 4/10",
-            job: {
-                job: "65eccc5ffc13ae7885cd3689"
-            }
-        },
-        {
-            body: "Meeting with a publisher for my republic book on tuesday",
-            job: {
-                job: "65eccc5ffc13ae7885cd368d"
-            }
-        }
-
-    ]
+const trackerSeeds = [
+  {
+    resume: {
+      sent: true,
+      date: new Date("03-01-2024"),
+      link: "https://resumeofgreece.com",
+    },
+    interviewDate: new Date("03-09-2024"),
+    interviewThankYou: {
+      email: "loremcontact@lorem.com",
+      sent: true,
+    },
+    interviewFollowUp: {
+      sent: false,
+      date: new Date("03-16-2024"),
+      email: "loremcontact@lorem.com",
+    },
+    other: {
+      title: "whyisthisrequired",
+      description: "whyisthisrequired",
+      completed: false,
+    },
+    notes: {
+      notes: ["65eccc5ffc13ae7885cd36c1"],
+    },
+    job: {
+      job: "65eccc5ffc13ae7885cd3689",
+    },
+  },
+  {
+    resume: {
+      sent: true,
+      date: new Date("02-26-2024"),
+      link: "https://socratesresume@example.com",
+    },
+    interviewDate: new Date("03-01-2024"),
+    interviewThankYou: {
+      email: "ipsumindustries@ipsum.com",
+      sent: true,
+    },
+    interviewFollowUp: {
+      sent: false,
+      date: new Date("03-25-2024"),
+      email: "ipsumindustries@ipsum.com",
+    },
+    other: {
+      title: "whyisthisrequired",
+      description: "whyisthisrequired",
+      completed: false,
+    },
+    notes: {
+      notes: ["65eccc5ffc13ae7885cd36c3"],
+    },
+    job: {
+      job: "65eccc5ffc13ae7885cd368d",
+    },
+  },
+];
