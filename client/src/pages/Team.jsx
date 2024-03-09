6ab4dae3951d0bee3637b85abbaf300edb6332ce
RB <jsriley24505@gmail.com>
2024-03-09 11:30:54 -0600

    added team members to team page. added a team scss

--- client/src/pages/Team.jsx
+++ client/src/pages/Team.jsx
@@ -2,9 +2,43 @@ function Team() {
 
 
     return (
-      <>
-  <h1>Meet the team that made this site possible!</h1>
-      </>
+      <div className="team">
+      <div className="row bg-stars">
+  <h1>Meet the team that JobPilot possible!</h1>
+<div className="row">
+  <h2>Jonathon Blom</h2>
+  <div className="col-md-6">
+  <a href="https://github.com/blomjonathon" className="btn btn-dark">Github</a>
+  <a href="https://65ea39a90d726d77d356f060--jonathonblom.netlify.app/" className="btn btn-dark">Portfolio</a>
+  </div>
+  </div>
+
+  <div className="row">
+  <h2>Riley Burns</h2>
+  <div className="col-md-6">
+  <a href="https://github.com/RB2277" className="btn btn-dark">Github</a>
+  <a href="https://main--superlative-otter-d6c03b.netlify.app/" className="btn btn-dark">Portfolio</a>
+  </div>
+  </div>
+
+
+  <div className="row">
+  <h2>Alex Geer</h2>
+  <div className="col-md-6">
+  <a href="https://github.com/agDesignz" className="btn btn-dark">Github</a>
+  <a href="https://alexgeer.netlify.app/" className="btn btn-dark">Portfolio</a>
+  </div>
+  </div>
+
+  <div className="row">
+  <h2>Meredith McDonald</h2>
+  <div className="col-md-6">
+  <a href="https://github.com/Ms-Meredith-McD" className="btn btn-dark">Github</a>
+  <a href="https://meredith-mcdonald-portfolio.netlify.app/" className="btn btn-dark">Portfolio</a>
+  </div>
+  </div>
+  </div>
+      </div>
     )
   }
   
