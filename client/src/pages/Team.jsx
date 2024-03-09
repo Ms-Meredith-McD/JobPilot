74ac559b452503b3dd4c4c358e13985ceb6c21ab
RB <jsriley24505@gmail.com>
2024-03-09 11:37:22 -0600

    centered elements on the team page

--- client/src/pages/Team.jsx
+++ client/src/pages/Team.jsx
@@ -2,37 +2,37 @@ function Team() {
 
 
     return (
-      <div className="team">
-      <div className="row bg-stars">
+      <div>
+      <div className="row bg-stars teamRow" id="teamContainer">
   <h1>Meet the team that JobPilot possible!</h1>
-<div className="row">
+<div className="row teamRow">
   <h2>Jonathon Blom</h2>
-  <div className="col-md-6">
+  <div className="col-md-6 teamCol">
   <a href="https://github.com/blomjonathon" className="btn btn-dark">Github</a>
   <a href="https://65ea39a90d726d77d356f060--jonathonblom.netlify.app/" className="btn btn-dark">Portfolio</a>
   </div>
   </div>
 
-  <div className="row">
+  <div className="row teamRow">
   <h2>Riley Burns</h2>
-  <div className="col-md-6">
+  <div className="col-md-6 teamCol">
   <a href="https://github.com/RB2277" className="btn btn-dark">Github</a>
   <a href="https://main--superlative-otter-d6c03b.netlify.app/" className="btn btn-dark">Portfolio</a>
   </div>
   </div>
 
 
-  <div className="row">
+  <div className="row teamRow">
   <h2>Alex Geer</h2>
-  <div className="col-md-6">
+  <div className="col-md-6 teamCol">
   <a href="https://github.com/agDesignz" className="btn btn-dark">Github</a>
   <a href="https://alexgeer.netlify.app/" className="btn btn-dark">Portfolio</a>
   </div>
   </div>
 
-  <div className="row">
+  <div className="row teamRow">
   <h2>Meredith McDonald</h2>
-  <div className="col-md-6">
+  <div className="col-md-6 teamCol">
   <a href="https://github.com/Ms-Meredith-McD" className="btn btn-dark">Github</a>
   <a href="https://meredith-mcdonald-portfolio.netlify.app/" className="btn btn-dark">Portfolio</a>
   </div>
