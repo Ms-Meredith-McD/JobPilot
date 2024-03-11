60e684432228626bd5a43a51a79ff04ddae992ae
Ms-Meredith-McD <meredith.e.mcdonald@gmail.com>
2024-03-11 14:22:20 -0500

    writing other.jsx

--- client/src/components/Other.jsx
+++ client/src/components/Other.jsx
@@ -1,65 +1,92 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import Button from "react-bootstrap/Button";
 import Form from "react-bootstrap/Form";
 
-export default function Other() {
+function Other() {
+    // Grouping state as an object
 
-    function submitOtherData(e) {
-        // Grouping state as an object
-        const [formState, setFormState] = useState({
-            title: "",
-            description: "",
-            boolean: false,
-        });
-        function handleSubmit(e) {
-            e.preventDefault();
-            setUserMessage("submitted");
-            setFormState({ title: "", description: "", boolean: "" });
+    const initialState = {
+        title: "",
+        description: "",
+        boolean: false,
+    };
+
+    const [formState, setFormState] = useState(initialState);
+    const [userMessage, setUserMessage] = useState("");
+
+    function handleInputChange(e) {
+        setFormState({ ...formState, [e.target.name]: e.target.value });
+    }
+
+    async function handleSubmit(e) {
+        e.preventDefault();
+        setUserMessage("submitted");
+
+        try {
+            const query = await fetch("/api/job", {
+                method: "POST",
+                body: JSON.stringify(formState),
+                headers: {
+                    "Content-Type": "application/json",
+                },
+            });
+            const result = await query.json;
+            console.log(formState);
+        } catch (error) {
+            console.log(error);
         }
 
-        return (
-            <>
-                <Form>
-                    <Form.Group className="mb-3" controlId="title">
-                        <Form.Control
-                            type="String"
-                            placeholder="Title"
-                            value={formState.title}
-                            name="title"
-                            onChange={handleInputChange}
-                            onBlur={handleValidation}
-                        />
-                    </Form.Group>
+        setFormState(initialState);
+    }
 
-                    <Form.Group className="mb-3" controlId="description">
-                        <Form.Control
-                            type="String"
-                            placeholder="Description"
-                            value={formState.description}
-                            name="description"
-                            onChange={handleInputChange}
-                            onBlur={handleValidation}
-                        />
-                    </Form.Group>
+    useEffect(() => {
+        userData && setFormState({ ...formState, user: userData._id });
+        console.log(formState);
+    }, [userData]);
 
-                    <Form.Group className="mb-3" controlId="completed">
-                        <Form.Control
-                            type="Boolean"
-                            placeholder="Completed"
-                            value={formState.completed}
-                            name="completed"
-                            onChange={handleInputChange}
-                            onBlur={handleValidation}
-                        />
-                    </Form.Group>
+    return (
+        <>
+            <Form>
+                <Form.Group className="mb-3" controlId="title">
+                    <Form.Control
+                        type="String"
+                        placeholder="Title"
+                        value={formState.title}
+                        name="title"
+                        onChange={handleInputChange}
+                        // onBlur={handleValidation}
+                    />
+                </Form.Group>
 
-                    <Button variant="primary" type="submit" onClick={handleSubmit}>
-                        Submit
-                    </Button>
-                    {userMessage && <h5 className="error-msg">{userMessage}</h5>}
-                </Form>
-            </>
+                <Form.Group className="mb-3" controlId="description">
+                    <Form.Control
+                        type="String"
+                        placeholder="Description"
+                        value={formState.description}
+                        name="description"
+                        onChange={handleInputChange}
+                        // onBlur={handleValidation}
+                    />
+                </Form.Group>
 
-        )
-    };
+                <Form.Group className="mb-3" controlId="completed">
+                    <Form.Control
+                        type="Boolean"
+                        placeholder="Completed"
+                        value={formState.completed}
+                        name="completed"
+                        onChange={handleInputChange}
+                        // onBlur={handleValidation}
+                    />
+                </Form.Group>
+
+                <Button variant="primary" type="submit" onClick={handleSubmit}>
+                    Submit
+                </Button>
+                {userMessage && <h5 className="error-msg">{userMessage}</h5>}
+            </Form>
+        </>
+
+    );
 }
+export default Other;
