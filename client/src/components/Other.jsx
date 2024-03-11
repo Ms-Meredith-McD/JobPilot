bd685e99d83badc7b2103b69db9b5332782f4c9b
Ms-Meredith-McD <meredith.e.mcdonald@gmail.com>
2024-03-11 18:58:37 -0500

    other.jsx

--- client/src/components/Other.jsx
+++ client/src/components/Other.jsx
@@ -1,94 +1,97 @@
 import { useState, useEffect } from "react";
-import Button from "react-bootstrap/Button";
 import Form from "react-bootstrap/Form";
+import useVerifyUser from "../hooks/useVerifyUser"
 
-export default function  Other() {
+export default function Other() {
+    const [otherData, setOtherData] = useState({});
 
-    function otherData () {
-    // Grouping state as an object
+    const [formMessage, setFormMessage] = useState("");
 
-    const [formState, setFormState] = useState("");({
-        title: "",
-        description: "",
-        boolean: false,
-    });
+    const { isLoggedIn, userData } = useVerifyUser();
 
-    const [userMessage, setUserMessage] = useState("");
-
-    function handleInputChange(e) {
-        setFormState({ ...formState, [e.target.name]: e.target.value });
-    }
-
-    async function handleSubmit(e) {
+    async function submitOtherData(e) {
         e.preventDefault();
-        setUserMessage("Submit successful");
-
+        console.log(otherData);
         try {
-            const query = await fetch("/api/other", {
+            //still needs to be setup with Alex's solution to grabbing users id (copied this from Riley's file)
+            const query = await fetch("/api/job/:id", {
                 method: "PUT",
-                body: JSON.stringify(formState),
+                body: JSON.stringify(otherData),
                 headers: {
                     "Content-Type": "application/json",
                 },
+            }).catch((err) => {
+                setFormMessage(
+                    err, "There was an issue creating your task."
+                );
             });
-            const result = await query.json;
-            console.log(formState);
-        } catch (error) {
-            console.log(error);
-        }
 
-        setFormState(initialState);
+            const result = await query.json();
+            if (result.status === "error") {
+                setFormMessage(
+                    "There was an issue creating your task."
+                );
+            } else {
+                // window.location.href = "/";
+            }
+        } catch (err) {
+            setFormMessage(
+                "We regret that we were unable to sign you up. Please try again."
+            );
+        }
+    }
+
+    function handleResumeChange(e) {
+        setFormMessage();
+        setResumeData({ ...otherData, [e.target.name]: e.target.value });
     }
 
     useEffect(() => {
-        userData && setFormState({ ...formState, user: userData._id });
-        console.log(formState);
+        userData && setOtherData({ ...otherData, user: userData._id });
+        console.log(otherData);
     }, [userData]);
 
     return (
         <>
-            <Form>
+            <Form className='otherForm' onSubmit={submitOtherData}>
                 <Form.Group className="mb-3" controlId="title">
+                    <Form.Label>Title</Form.Label>
                     <Form.Control
                         type="String"
                         placeholder="Title"
-                        value={formState.title}
-                        name="title"
-                        onChange={handleInputChange}
-                        // onBlur={handleValidation}
-                    />
+                        name='title'
+                        aria-describedby='otherTitle'
+                        value={otherData?.title || ""}
+                        onChange={handleOtherChange} />
                 </Form.Group>
-
                 <Form.Group className="mb-3" controlId="description">
+                    <Form.Label>Description</Form.Label>
                     <Form.Control
                         type="String"
                         placeholder="Description"
-                        value={formState.description}
-                        name="description"
-                        onChange={handleInputChange}
-                        // onBlur={handleValidation}
-                    />
+                        name='description'
+                        aria-describedby='otherDescription'
+                        value={otherData?.description || ""}
+                        onChange={handleOtherChange} />
                 </Form.Group>
-
                 <Form.Group className="mb-3" controlId="completed">
+                    <Form.Label>Completed</Form.Label>
                     <Form.Control
                         type="Boolean"
                         placeholder="Completed"
-                        value={formState.completed}
-                        name="completed"
-                        onChange={handleInputChange}
-                        // onBlur={handleValidation}
-                    />
+                        name='completed'
+                        aria-describedby='otherCompleted'
+                        value={otherData?.completed || ""}
+                        onChange={handleOtherChange} />
                 </Form.Group>
-
-                <Button variant="primary" type="submit" onClick={handleSubmit}>
-                    Submit
-                </Button>
-                {userMessage && <h5 className="error-msg">{userMessage}</h5>}
             </Form>
-        </>
 
+            {formMessage && formMessage.length > 0 && (
+                <div className="row">
+                    <div className="col-12">{formMessage}</div>
+                </div>
+            )}
+        </>
     );
-    }
 }
 
