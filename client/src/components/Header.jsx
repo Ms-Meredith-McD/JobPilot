d2b7bdd1d295ba87a3ad4ecbc3d0525c69d6fd55
Jonathon Blom <blomjonathon@gmail.com>
2024-03-11 14:10:11 -0500

    improved nav bar

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -1,12 +1,8 @@
 import useVerifyUser from "../hooks/useVerifyUser";
 import { Link } from "react-router-dom";
-import Button from 'react-bootstrap/Button';
-import Container from 'react-bootstrap/Container';
-import Form from 'react-bootstrap/Form';
-import Nav from 'react-bootstrap/Nav';
-import Navbar from 'react-bootstrap/Navbar';
-import NavDropdown from 'react-bootstrap/NavDropdown';
-import Offcanvas from 'react-bootstrap/Offcanvas';
+import Container from "react-bootstrap/Container";
+import Nav from "react-bootstrap/Nav";
+import Navbar from "react-bootstrap/Navbar";
 
 function Header() {
   const { isLoggedIn, logout } = useVerifyUser();
@@ -25,7 +21,7 @@ function Header() {
             aria-controls="navbarNav"
             aria-expanded="false"
             aria-label="Toggle navigation"
-            onClick={()=> {OffcanvasExample()}}
+            onClick={() => {}}
           >
             <span className="navbar-toggler-icon"></span>
           </button>
@@ -67,66 +63,56 @@ function Header() {
   );
 }
 
-export default Header;
-
-
-
-
-
-
-
+export default OffcanvasExample;
 
 function OffcanvasExample() {
+  const { isLoggedIn, logout } = useVerifyUser();
+
   return (
     <>
-      {[false, 'sm', 'md', 'lg', 'xl', 'xxl'].map((expand) => (
-        <Navbar key={expand} expand={expand} className="bg-body-tertiary mb-3">
+      <div className="d-flex">
+        <Navbar className="navbar navbar-dark w-100 p-2 mb-3" expand={false}>
           <Container fluid>
-            <Navbar.Brand href="#">Navbar Offcanvas</Navbar.Brand>
-            <Navbar.Toggle aria-controls={`offcanvasNavbar-expand-${expand}`} />
-            <Navbar.Offcanvas
-              id={`offcanvasNavbar-expand-${expand}`}
-              aria-labelledby={`offcanvasNavbarLabel-expand-${expand}`}
-              placement="end"
-            >
-              <Offcanvas.Header closeButton>
-                <Offcanvas.Title id={`offcanvasNavbarLabel-expand-${expand}`}>
-                  Offcanvas
-                </Offcanvas.Title>
-              </Offcanvas.Header>
-              <Offcanvas.Body>
-                <Nav className="justify-content-end flex-grow-1 pe-3">
-                  <Nav.Link href="#action1">Home</Nav.Link>
-                  <Nav.Link href="#action2">Link</Nav.Link>
-                  <NavDropdown
-                    title="Dropdown"
-                    id={`offcanvasNavbarDropdown-expand-${expand}`}
-                  >
-                    <NavDropdown.Item href="#action3">Action</NavDropdown.Item>
-                    <NavDropdown.Item href="#action4">
-                      Another action
-                    </NavDropdown.Item>
-                    <NavDropdown.Divider />
-                    <NavDropdown.Item href="#action5">
-                      Something else here
-                    </NavDropdown.Item>
-                  </NavDropdown>
-                </Nav>
-                <Form className="d-flex">
-                  <Form.Control
-                    type="search"
-                    placeholder="Search"
-                    className="me-2"
-                    aria-label="Search"
-                  />
-                  <Button variant="outline-success">Search</Button>
-                </Form>
-              </Offcanvas.Body>
-            </Navbar.Offcanvas>
+            <Navbar.Brand>
+              <Link className="navbar-brand" to="/">
+                JobPilot
+              </Link>
+            </Navbar.Brand>
+            <Navbar.Toggle aria-controls="responsive-navbar-nav" />
+            <Navbar.Collapse id="responsive-navbar-nav">
+              <Nav className="me-auto">
+                <ul className="navbar-nav">
+                  <li className="nav-item active">
+                    <Link className="nav-link" to="/jobs">
+                      My Jobs
+                    </Link>
+                  </li>
+                  <li className="nav-item">
+                    <Link className="nav-link" to="/resume">
+                      Resumes
+                    </Link>
+                  </li>
+                  <li className="nav-item">
+                    {isLoggedIn === true ? (
+                      <button
+                        type="button"
+                        className="btn btn-light"
+                        onClick={logout}
+                      >
+                        Logout
+                      </button>
+                    ) : (
+                      <Link type="button" className="btn btn-light" to="/login">
+                        Login or signup please!
+                      </Link>
+                    )}
+                  </li>
+                </ul>
+              </Nav>
+            </Navbar.Collapse>
           </Container>
         </Navbar>
-      ))}
+      </div>
     </>
   );
 }
-
