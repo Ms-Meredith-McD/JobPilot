b2c2f2e597d18eba58d9c656b6293a163e275354
Ms-Meredith-McD <meredith.e.mcdonald@gmail.com>
2024-03-12 11:43:28 -0500

    working on links

--- client/src/pages/Resources.jsx
+++ client/src/pages/Resources.jsx
@@ -1,7 +1,7 @@
+import { Link } from 'react-router-dom';
 import '../styles/components/_resources.scss';
 
-
-const Resume = () => {
+const Resources = () => {
     return (
         <section className="page-section bg-stars">
             <div className="container">
@@ -9,18 +9,18 @@ const Resume = () => {
                     <div className="resume-column">
                         <h2>Resume Templates:</h2>
                         <ul className="list">
-                            <li><a href="../assets/entrylevelResume.pdf" download>Entry Level Resume</a></li>
-                            <li><a href="../assets/midlevelResume.pdf" download>Mid Career Resume</a></li>
-                            <li><a href="../assets/advancedResume.pdf" download>Advanced Career Resume</a></li>
+                            <li><Link to="../assets/entrylevelResume.pdf" target="_blank" download>Entry Level Resume</Link></li>
+                            <li><Link to="../assets/midlevelResume.pdf" target="_blank" download>Mid Career Resume</Link></li>
+                            <li><Link to="../assets/advancedResume.pdf" target="_blank" download>Advanced Career Resume</Link></li>
                         </ul>
                     </div>
                     <div className="resume-column">
-                        <h2>My Resumes:</h2>
+                        <h2>Cover Letter Resources:</h2>
                         <ul className="list">
-                            <li>cover letter link placeholder</li>
-                            <li>cover letter link placeholder</li>
-                            <li>cover letter link placeholder</li>
-                            <li>cover letter link placeholder</li>
+                            <li><Link to="https://www.indeed.com/career-advice/resumes-cover-letters/free-cover-letter">Indeed</Link></li>
+                            <li><Link to="https://resumegenius.com/cover-letter-templates/basic-simple-templates">Resume Genius</Link></li>
+                            <li><Link to="https://www.canva.com/create/cover-letters/">Canva</Link></li>
+                            <li><Link to="https://docs.google.com/document/d/1a4yU8rWtT1DWTS7H_c9EguUyFt49B2qU92demxvozTQ/edit">Google Docs</Link></li>
                         </ul>
                     </div>
                 </div>
@@ -29,4 +29,4 @@ const Resume = () => {
     );
 };
 
-export default Resume;
+export default Resources;
