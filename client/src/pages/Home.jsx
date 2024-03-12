b34dac53d4ab20186f4d5cbf0a34f74c97d31b2b
Jonathon Blom <blomjonathon@gmail.com>
2024-03-12 13:32:28 -0500

    made some final adjustments to hero image

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -5,13 +5,13 @@ function Home() {
       <div className="bg-stars homePage">
         <h1> Welcome to JobPilot</h1>
         <h2>Are you ready for liftoff?</h2>
-      <div className="hero" style={{ backgroundImage: `url(${hero})` }}></div>
         <p>
           JobPilot features a fullly functional job tracker to aid you in your
           search for a job. JobPilot has features such as being able to remember
           your interview dates, reminders to send thank you emails to your
           interviewers, and so much more!{" "}
         </p>
+        <div className="hero" style={{ backgroundImage: `url(${hero})` }}></div>
       </div>
     </>
   );
