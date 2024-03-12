739779d46b751734b20941e7b9add9e108e3a253
Ms-Meredith-McD <meredith.e.mcdonald@gmail.com>
2024-03-12 09:14:50 -0500

    fixed my model

--- server/models/Other.js
+++ server/models/Other.js
@@ -2,12 +2,9 @@ const mongoose = require('mongoose');
 
 
 const otherSchema = new mongoose.Schema({
-    other: {
-        title: { type: String, required: true },
+        title: {type: String, required: true },
         description: { type: String, required: true },
         completed: { type: Boolean }
-
-    }
     });
 
 
