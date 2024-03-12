0d84eb0f53703ff5654f09de4c4327644f95fe6a
RB <jsriley24505@gmail.com>
2024-03-11 19:09:01 -0500

    began to work on home page

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -2,9 +2,13 @@ function Home() {
 
 
   return (
-    <>
-<h1> Home</h1>
-    </>
+    <div className="bg-stars">
+      <div className="hero"></div>
+<h1> Welcome to JobPilot</h1>
+<h2>Are you ready for liftoff?</h2>
+<p>JobPilot features a fullly functional job tracker to aid you in your search for a job. JobPilot has features such as being able to remember your interview dates, reminders to send
+  thank you emails to your interviewers, and so much more! </p>
+    </div>
   )
 }
 
